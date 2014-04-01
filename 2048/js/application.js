52949847212aeb233aa4fb22bd52251a089bda11
uchihatmtkinu <uchihawzy@163.com>
2014-04-02 00:24:36 +0800

    Fix of size

--- 2048/js/application.js
+++ 2048/js/application.js
@@ -1,6 +1,6 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager,0);
+  new GameManager(5, KeyboardInputManager, HTMLActuator, LocalScoreManager,0);
 
 });
 
